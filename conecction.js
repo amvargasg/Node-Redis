97036b3bc8761cdbb16574a4a6e7013e6774eb8f
Maria Paula Mora <mpaulamora@ibm.com>
2021-05-07 16:05:04 -0500

    actualizando el password para coneccion

--- conecction.js
+++ conecction.js
@@ -3,7 +3,8 @@ const redis = require("redis");
 const connection = 
 { 
     port      : 6379,                 // replace with your port
-    host      : 'redis.integracion.svc.cluster.local'       // replace with your hostanme or IP address
+    host      : 'redis.integracion.svc.cluster.local',      // replace with your hostanme or IP address
+    password: 'admin'
 } ;
 
 const client = redis.createClient(connection);
