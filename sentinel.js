cbb11e25a2d89a38fbb2b4e0688c0998d5e2da93
mariaelisacf <82952419+mariaelisacf@users.noreply.github.com>
2021-05-07 09:43:44 -0500

    Update sentinel.js

--- sentinel.js
+++ sentinel.js
@@ -6,14 +6,14 @@ const endpoints = [
     {host: 'redis-sentinel', port: 5000}
 ];
 
-//const opts = {}; // Standard node_redis client options
-//const masterName = 'mymaster';
+const opts = {}; // Standard node_redis client options
+const masterName = 'mymaster';
 
 
 
 // masterName and opts are optional - masterName defaults to 'mymaster'
-//const redisClient = sentinel.createClient(endpoints, masterName, opts);
-const redisClient = sentinel.createClient(endpoints, 'master', {})
+const redisClient = sentinel.createClient(endpoints, masterName, opts);
+
 
 redisClient.on("connect", function() {
     console.log("You are now connected");
@@ -25,6 +25,6 @@ module.exports = {
 };
 
 // The master is the default case if no role is specified.
-//const masterClient = sentinel.createClient(endpoints, masterName, {role: 'master'}); 
-//const slaveClient = sentinel.createClient(endpoints, masterName, {role: 'slave'});
-//const sentinelClient = sentinel.createClient(endpoints, {role: 'sentinel'});
+const masterClient = sentinel.createClient(endpoints, masterName, {role: 'master'}); 
+const slaveClient = sentinel.createClient(endpoints, masterName, {role: 'slave'});
+const sentinelClient = sentinel.createClient(endpoints, {role: 'sentinel'});
