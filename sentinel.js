33803f871fbd47c3ae79b46eb15f2623a0d34d33
Maria Paula Mora <mpaulamora@ibm.com>
2021-05-10 16:12:01 -0500

    add no_ready_check

--- sentinel.js
+++ sentinel.js
@@ -2,16 +2,17 @@ const sentinel = require('redis-sentinel');
  
 // List the sentinel endpoints
 const endpoints = [
-    {port: 6379, host: 'redis.integracion.svc.cluster.local'}, //redis cluster
-    {port: 5000, host: 'redis-sentinel.integracion.svc.cluster.local'}
+    {port: 6379, host: 'redis.integracion.svc.cluster.local', password:'admin'}, //redis cluster
+    {port: 5000, host: 'redis-sentinel.integracion.svc.cluster.local', password:'admin'}
 ];
  
 const opts = {}; // Standard node_redis client options
 const masterName = 'mymaster';
-//const auth_pass= 'admin';
+const no_ready_check = true;
+const auth_pass= 'admin';
  
 // masterName and opts are optional - masterName defaults to 'mymaster'
-const redisClient = sentinel.createClient(endpoints, masterName, opts);
+const redisClient = sentinel.createClient(endpoints, masterName, opts, no_ready_check, auth_pass);
 
 redisClient.on("connect", function() {
     console.log("Connected");
