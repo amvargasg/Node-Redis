a8b5230a8a2e09bec866ba25507de3705f8b21da
Maria Paula Mora <mpaulamora@ibm.com>
2021-05-10 14:38:58 -0500

    cambio de puerto redissentinel antes:5000 ahora:26379

--- sentinel.js
+++ sentinel.js
@@ -3,16 +3,16 @@ const sentinel = require('redis-sentinel');
 // List the sentinel endpoints
 const endpoints = [
     {port: 6379, host: 'redis.integracion.svc.cluster.local'}, //redis cluster
-    {port: 5000, host: 'redis-sentinel.integracion.svc.cluster.local'}
+    {port: 26379, host: 'redis-sentinel.integracion.svc.cluster.local'}
    // {host: 'redis-sentinel', port:'redis.integracion.svc.cluster.local'}
 ];
  
 const opts = {}; // Standard node_redis client options
 const masterName = 'mymaster';
-//const auth_pass= 'admin';
+const auth_pass= 'admin';
  
 // masterName and opts are optional - masterName defaults to 'mymaster'
-const redisClient = sentinel.createClient(endpoints, masterName, opts);
+const redisClient = sentinel.createClient(endpoints, masterName, opts, auth_pass);
 
 redisClient.on("connect", function() {
     console.log("Connected");
